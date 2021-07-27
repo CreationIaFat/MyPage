81a5dc778067608a1c07563b5b9094b21040bb7b
HiguchiMan <higuchiman1995>
2021-07-28 00:39:31 +0900

    fix dir

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   resolve: {
     // @/ によるimportをできるようにする。
     alias: {
-      '@': path.resolve('__dirname', '/src')
+      '@': path.resolve('__dirname', '/MyPage/src')
     }
   },
   build: {
